f05efed54997312b3bdc479d76e28b29d561d627
fabricelg <fabrice.lg@gmail.com>
2018-10-31 14:25:48 +0100

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,1 @@
-export const environment = {
-  production: true,
-  firebase: {
-    apiKey: "AIzaSyCpAzpGaakICeJY3Sy-BL-Wvhpn80VioUw",
-    authDomain: "bddplongee.firebaseapp.com",
-    databaseURL: "https://bddplongee.firebaseio.com",
-    projectId: "bddplongee",
-    storageBucket: "",
-    messagingSenderId: "170145982103",
-  }
-};
+export const environment = {};
